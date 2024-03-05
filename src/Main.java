c9f8124e81a0494e75d6359847c93ddd8ef26331
KOV <114916256+OscarKhonma@users.noreply.github.com>
2024-03-05 16:10:37 +0300

    fixed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
+    static String input;
+    static String[] myList;
     static char operation;
     static String firstArgument;
     static String secondArgument;
@@ -8,52 +10,75 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String[] myList = new String[10];
-        if (input.contains("+")){
-            myList = input.split(" \\+ ");
-            operation = '+';
-        }else if (input.contains("-")){
-            myList = input.split(" - ");
-            operation = '-';
-        }else if (input.contains("/")){
-            myList = input.split(" / ");
-            operation = '/';
-        }else if (input.contains("*")){
-            myList = input.split(" \\* ");
-            operation = '*';
-        }
+        input = scanner.nextLine();
+
+        defineTheOperation();
+
         firstArgument = myList[0].trim();
         secondArgument = myList[1].trim();
 
+        resolution();
+    }
+
+
+    static void resolution(){
         if(myList.length == 2){
             if (operation == '+' || operation == '-'){
-                if ((firstArgument.contains("\"")) && (secondArgument.contains("\""))){
-                    firstArgument = firstArgument.replaceAll("\"", "");
-                    secondArgument = secondArgument.replaceAll("\"", "");
-                    if (firstArgument.length() <= 10 && secondArgument.length() <= 10){
-                        System.out.println("\"" + getResult() + "\"");
-                    }else throw new RuntimeException("Длина строк превышает допустимое значение, длина строк не должна превышать 10 символов!");
-                }else throw new RuntimeException("Для сложения нужны строки в кавычках!");
+                isPlusOrMinusCalc();
             }
             else if(operation =='/' && !(secondArgument.contains("\"")) && (firstArgument.contains("\""))||
-            operation == '*' && !(secondArgument.contains("\"")) && (firstArgument.contains("\""))){
-                firstArgument = firstArgument.replaceAll("\"", "");
-                secondArgument = secondArgument.replaceAll("\"", "");
-                if (firstArgument.length() <= 10 && isParsable(secondArgument)) {
-                    if (Integer.parseInt(secondArgument) >= 1 && Integer.parseInt(secondArgument) <= 10) {
-                        if (getResult().length() > 40) {
-                            result = getResult().substring(0, 40);
-                            System.out.println("\"" + result + "\"");
-                        }
-                        else {
-                            System.out.println("\"" + getResult() + "\"");
-                        }
-                    }else throw new RuntimeException("Число не может быть меньше 0 и больше 10!");
-                }else throw new RuntimeException("Строка не можешт превышать 10 символов. Строку нужно умножать или делить только на целое число!");
+                    operation == '*' && !(secondArgument.contains("\"")) && (firstArgument.contains("\""))){
+                isMultiplyOrDivCalc();
             }else throw new RuntimeException("Не допустимое значение операции!");
         }else throw new RuntimeException("Выражения должно быть два");
     }
+
+
+    static void defineTheOperation(){
+        if (input.contains(" + ")){
+            myList = input.split(" \\+ ");
+            operation = '+';
+        }else if (input.contains(" - ")){
+            myList = input.split(" - ");
+            operation = '-';
+        }else if (input.contains(" / ")){
+            myList = input.split(" / ");
+            operation = '/';
+        }else if (input.contains(" * ")){
+            myList = input.split(" \\* ");
+            operation = '*';
+        }
+    }
+
+
+    static void isPlusOrMinusCalc(){
+        if ((firstArgument.contains("\"")) && (secondArgument.contains("\""))){
+            firstArgument = firstArgument.replaceAll("\"", "");
+            secondArgument = secondArgument.replaceAll("\"", "");
+            if (firstArgument.length() <= 10 && secondArgument.length() <= 10){
+                System.out.println("\"" + getResult() + "\"");
+            }else throw new RuntimeException("Длина строк превышает допустимое значение, длина строк не должна превышать 10 символов!");
+        }else throw new RuntimeException("Для сложения нужны строки в кавычках!");
+    }
+
+
+    static void isMultiplyOrDivCalc(){
+        firstArgument = firstArgument.replaceAll("\"", "");
+        secondArgument = secondArgument.replaceAll("\"", "");
+        if (firstArgument.length() <= 10 && isParsable(secondArgument)) {
+            if (Integer.parseInt(secondArgument) >= 1 && Integer.parseInt(secondArgument) <= 10) {
+                if (getResult().length() > 40) {
+                    result = getResult().substring(0, 40);
+                    System.out.println("\"" + result + "...\"");
+                }
+                else {
+                    System.out.println("\"" + getResult() + "\"");
+                }
+            }else throw new RuntimeException("Число не может быть меньше 0 и больше 10!");
+        }else throw new RuntimeException("Строка не можешт превышать 10 символов. Строку нужно умножать или делить только на целое число!");
+    }
+
+
     public static boolean isParsable(String input){
         try{
             Integer.parseInt(input);
@@ -66,11 +91,12 @@ public class Main {
         return result = calc(firstArgument, secondArgument, operation);
     }
 
+
     public static String calc(String arg1, String arg2, char operation){
         return switch (operation){
             case '+' -> arg1.concat(arg2);
             case '-' -> arg1.replaceFirst(arg2, "");
-            case '/' -> arg1.substring(0, (int)(arg1.length()/Integer.parseInt(arg2)));
+            case '/' -> arg1.substring(0, (arg1.length()/Integer.parseInt(arg2)));
             case '*' -> arg1.repeat(Integer.parseInt(arg2));
             default -> throw new IllegalStateException("Unexpected value: " + operation);
         };
@@ -85,14 +111,11 @@ public class Main {
 //    public static void main(String[] args) {
 //        Scanner scanner = new Scanner(System.in);
 //        String input = scanner.nextLine();
-//        String ch =  scanner.nextLine();
-//        char ch1 = ch.charAt(0);
-//        int result = sum(input, ch1);
-//        System.out.println(result);
+//        char ch =  scanner.nextLine().charAt(0);
+//        System.out.println(sum(input, ch));
 //
 //    }
 //    public static int sum(String x, char y){
-//        char[] forOperationArray = new char[20];
 //        int count = 0;
 //        for (int i = 0; i<x.length(); i++){
 //            if (x.charAt(i) == y){
